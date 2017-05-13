2474605735f8f992533eac345f71b7df61a3e0fc
Sergio Mendoza <smendoza787@gmail.com>
2017-05-12 22:24:47 -0700

    check for email and username uniqueness

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,5 +1,5 @@
 class Festival < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :location, presence: true
   validates :date, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true
-  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 
   has_secure_password
