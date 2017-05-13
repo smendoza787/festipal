8b7dac130c64bff0dbd310855a866b9b0c2e60f9
Sergio Mendoza <smendoza787@gmail.com>
2017-05-12 17:51:41 -0700

    revert before pg

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require 'rack-flash'
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.slite"
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
 require_all 'app'
