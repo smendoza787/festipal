ca32aab8a1e72186e3111762a4cc6a7e12e33a98
Sergio Mendoza <smendoza787@gmail.com>
2017-05-12 21:22:17 -0700

    added random bg image to headers, added random messages for empty profiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,5 +22,35 @@ class ApplicationController < Sinatra::Base
     def logged_in?
       !!current_user
     end
+
+    def profile_random_string
+      arr = [
+        "...looks like they have better things to do. :/",
+        "...work or something, I hope.",
+        "...you look like you need a music festival in your life.",
+        "...do you know what it feeeeels like, loving someone that's in a rush to throw you away?",
+        "...let's go to a festival!",
+        "You need to get out more...I know, a festival!",
+        "I know school is important but you should at least get a festival in or two...",
+        "...nothing, yet!",
+        "...school young man!",
+        "A festival a day keeps the bad vibes at bay."
+      ]
+
+      arr[rand(0..arr.size-1)]
+    end
+
+    def random_bg_image_url
+      arr = [
+        "/images/festival1.jpeg",
+        "/images/festival2.jpeg",
+        "/images/festival3.jpeg",
+        "/images/festival4.jpeg",
+        "/images/festival5.jpg"
+      ]
+
+      arr[rand(0..arr.size-1)]
+    end
+
   end
 end
